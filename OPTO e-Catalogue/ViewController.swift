3c428f4ebddb8cf7a01314fdc9f09b52dc604bb7
LightningDev <User@192-168-1-8.tpgi.com.au>
2016-11-08 18:24:24 +1000

    Initial Commit

---
+++ OPTO e-Catalogue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OPTO e-Catalogue
+//
+//  Created by Nhat Tran on 8/11/2016.
+//  Copyright © 2016 TedBinary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
